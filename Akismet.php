d2342a78b525292b45332140a724f0ee1420b90f
三三 <oott123@users.noreply.github.com>
2015-09-28 12:00:57 +0800

    disable support for the @ prefix for uploading files

--- Akismet.php
+++ Akismet.php
@@ -110,6 +110,7 @@ class Akismet
         $options[CURLOPT_TIMEOUT] = (int) $this->getTimeOut();
         $options[CURLOPT_POST] = true;
         $options[CURLOPT_POSTFIELDS] = $aParameters;
+        $options[CURLOPT_SAFE_UPLOAD] = true;
 
         // speed up things, use HTTP 1.0
         $options[CURLOPT_HTTP_VERSION] = CURL_HTTP_VERSION_1_0;
