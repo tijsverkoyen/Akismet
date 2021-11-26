73d6ce4fdae8a5d1391603959a54b47302ab2696
Tijs Verkoyen <tijs@verkoyen.eu>
2021-11-26 14:20:22 +0100

    Fix tiny codestyling issue

--- Akismet.php
+++ Akismet.php
@@ -156,7 +156,7 @@ class Akismet
         }
 
         // error?
-        if ((int)$errorNumber > 0) {
+        if ((int) $errorNumber > 0) {
             throw new Exception($errorMessage, $errorNumber);
         }
 
